a7da01a94d89034a68643938129460dba0a2915a
1Batrex1 <bartosz.gajda@protonmail.com>
2023-01-24 12:22:22 +0100

    Alpha 0.00001

---
+++ Warhammer/Warhammer.py
@@ -0,0 +1,14 @@
+class Hero:
+
+    def __init__(self,n,r,p,cd,Wep,Ski,Tal,Eq,M):
+        self.name = n
+        self.race = r
+        self.profession = p
+        self.Chad = cd
+        self.wep = []
+        self.wep.append(Wep)
+        self.st = []
+        
+
+
+
