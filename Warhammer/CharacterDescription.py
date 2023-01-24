3f48512e7996cd6644e94078f68ed5f99a487e01
1Batrex1 <bartosz.gajda@protonmail.com>
2023-01-24 14:31:48 +0100

    Alpha 0.00009

--- Warhammer/CharacterDescription.py
+++ Warhammer/CharacterDescription.py
@@ -1,46 +1,48 @@
 import random
+
+
 class CharacterDescription:
-    def __init__(self, gender,race):
-        age_rand = random.randint(1,100)
-        #Losowanie wieku
+    def __init__(self, gender, race):
+        age_rand = random.randint(1, 100)
+        # Losowanie wieku
         if race == "Człowiek":
             if age_rand <= 5:
                 self.age = 16
-            elif age_rand <=10:
+            elif age_rand <= 10:
                 self.age = 17
-            elif age_rand <=15:
+            elif age_rand <= 15:
                 self.age = 18
-            elif age_rand <=20:
+            elif age_rand <= 20:
                 self.age = 19
-            elif age_rand <=25:
+            elif age_rand <= 25:
                 self.age = 20
-            elif age_rand <=30:
+            elif age_rand <= 30:
                 self.age = 21
-            elif age_rand <=35:
+            elif age_rand <= 35:
                 self.age = 22
-            elif age_rand <=40:
+            elif age_rand <= 40:
                 self.age = 23
-            elif age_rand <=45:
+            elif age_rand <= 45:
                 self.age = 24
-            elif age_rand <=50:
+            elif age_rand <= 50:
                 self.age = 25
-            elif age_rand <=55:
+            elif age_rand <= 55:
                 self.age = 26
-            elif age_rand <=60:
+            elif age_rand <= 60:
                 self.age = 27
-            elif age_rand <=65:
+            elif age_rand <= 65:
                 self.age = 28
-            elif age_rand <=70:
+            elif age_rand <= 70:
                 self.age = 29
-            elif age_rand <=75:
+            elif age_rand <= 75:
                 self.age = 30
-            elif age_rand <=80:
+            elif age_rand <= 80:
                 self.age = 31
-            elif age_rand <=85:
+            elif age_rand <= 85:
                 self.age = 32
-            elif age_rand <=90:
+            elif age_rand <= 90:
                 self.age = 33
-            elif age_rand <=95:
+            elif age_rand <= 95:
                 self.age = 34
             else:
                 self.age = 35
@@ -168,26 +170,26 @@ class CharacterDescription:
             else:
                 self.age = 60
 
-        #Losowanie Wzrostu
-        height_rand = random.randint(1,10) + random.randint(1,10)
+        # Losowanie Wzrostu
+        height_rand = random.randint(1, 10) + random.randint(1, 10)
         if race == "Człowiek":
-            self.height= 160 + height_rand
+            self.height = 160 + height_rand
             if gender == "Kobieta":
                 self.height -= 10
         elif race == "Elf":
-            self.height= 170 + height_rand
+            self.height = 170 + height_rand
             if gender == "Kobieta":
                 self.height -= 10
         elif race == "Krasnolud":
-            self.height= 145 + height_rand
+            self.height = 145 + height_rand
             if gender == "Kobieta":
                 self.height -= 15
         elif race == "Niziołek":
-            self.height= 110 + height_rand
+            self.height = 110 + height_rand
             if gender == "Kobieta":
                 self.height -= 10
-        #Losowanie Koloru Oczu
-        eye_rand = random.randint(1,10) + random.randint(1,10)
+        # Losowanie Koloru Oczu
+        eye_rand = random.randint(1, 10) + random.randint(1, 10)
         if race == "Człowiek":
             if eye_rand == 1:
                 self.eye_color = "Szary"
@@ -257,8 +259,8 @@ class CharacterDescription:
             elif eye_rand <= 10:
                 self.eye_color = "Ciemnobrązowy"
 
-        #Losowanie Znaku szczególnego
-        distinctive_sign_rand = random.randint(1,100)
+        # Losowanie Znaku szczególnego
+        distinctive_sign_rand = random.randint(1, 100)
         if distinctive_sign_rand <= 5:
             self.distinctive_sign = "Bielmo na oku"
         elif distinctive_sign_rand <= 10:
@@ -344,3 +346,49 @@ class CharacterDescription:
             self.star_sign = "Wymund Pustelnik"
         else:
             self.star_sign = "Złoty Kogut"
+        number_of_siblings_rand = random.randint(1, 10)
+
+        if race == "Człowiek":
+            if number_of_siblings_rand == 1:
+                self.number_of_siblings = 0
+            elif number_of_siblings_rand <= 3:
+                self.number_of_siblings = 1
+            elif number_of_siblings_rand <= 5:
+                self.number_of_siblings = 2
+            elif number_of_siblings_rand <= 7:
+                self.number_of_siblings = 3
+            elif number_of_siblings_rand <= 9:
+                self.number_of_siblings = 4
+            else:
+                self.number_of_siblings = 5
+        elif race == "Elf":
+            if number_of_siblings_rand == 1:
+                self.number_of_siblings = 0
+            elif number_of_siblings_rand <= 5:
+                self.number_of_siblings = 1
+            elif number_of_siblings_rand <= 9:
+                self.number_of_siblings = 2
+            else:
+                self.number_of_siblings = 3
+        elif race == "Krasnolud":
+            if number_of_siblings_rand <= 3:
+                self.number_of_siblings = 0
+            elif number_of_siblings_rand <= 7:
+                self.number_of_siblings = 1
+            elif number_of_siblings_rand <= 9:
+                self.number_of_siblings = 2
+            else:
+                self.number_of_siblings = 3
+        elif race == "Niziołek":
+            if number_of_siblings_rand == 1:
+                self.number_of_siblings = 1
+            elif number_of_siblings_rand <= 3:
+                self.number_of_siblings = 2
+            elif number_of_siblings_rand <= 5:
+                self.number_of_siblings = 3
+            elif number_of_siblings_rand <= 7:
+                self.number_of_siblings = 4
+            elif number_of_siblings_rand <= 9:
+                self.number_of_siblings = 5
+            else:
+                self.number_of_siblings = 6
