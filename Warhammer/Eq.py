e6c448d5e626ccca4fc4e8802db5393f53d51d40
1Batrex1 <bartosz.gajda@protonmail.com>
2023-01-24 12:51:07 +0100

    Alpha 0.00003

---
+++ Warhammer/Eq.py
@@ -0,0 +1,8 @@
+class Eq:
+
+    def __init__(self,name,description):
+        self.name = name
+        self.description = description
+
+    def GetEq(self):
+        return self.name , self.description
--- Warhammer/Hero.py
+++ Warhammer/Hero.py
@@ -4,15 +4,20 @@
 class Hero:
 
 
-    class Sklis:
+    class Skill:
         def  __init__(self,n,s1,s2):
             self.name = n #nazwa skila
             self.spec1 = s1 #czy specjalizacja +10%
             self.spec2 = s1 #czy specjalizacja +10%
+        def GetSkill(self):
+            return self.name + self.spec1 + self.spec2
+        
     class Talents:
         def  __init__(self,n,desc):
             self.name = n #nazwa talenty
             self.description = desc # opis umiejętności
+        def GetTalent(self):
+            return self.name + self.description
 
 
 
